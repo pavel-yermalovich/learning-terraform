bee9bb499cab5ec134878de276f847442f8aa631
Pavel Yermalovich <x48ray@gmail.com>
2023-12-22 19:21:21 +0100

    delete extra listeners

--- main.tf
+++ main.tf
@@ -112,18 +112,6 @@ module "alb" {
     }
   }
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-  }
-
   tags = {
     Environment = "dev"
   }
